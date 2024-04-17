af087c9d1eff842ce1bd6463fb9308d5d7e595b3
aryannaik225 <ary.nai.rt22@dypatil.edu>
2024-04-18 02:15:22 +0530

    running-changes

--- whatcan.py
+++ whatcan.py
@@ -2,17 +2,7 @@ def readme():
     response = """
     I'm your AI assistant. Here are some things I can do:
 
-    1. Set alarms.
-    2. Open and manage GUI windows.
-    3. Play games like Rock, Paper, Scissors.
-    4. Control video playback.
-    5. Search on Google, YouTube, and Wikipedia.
-    6. Get temperature and weather information.
-    7. Remember user input.
-    8. Display latest news headlines.
-    9. Perform calculations.
-    10. Send messages on WhatsApp.
-    11. And many more tasks!
+    Set alarms, Open and manage GUI windows, Play games, Control video playback, Search on Google, YouTube, and Wikipedia, Get temperature information, Display latest news headlines, Perform calculations, Send messages on WhatsApp, And many more tasks!
 
     Feel free to ask for help with any of these tasks!
     """
