10f82dbf277cd54a48e393be8d471db63bfc2133
aryannaik225 <ary.nai.rt22@dypatil.edu>
2024-04-18 00:52:40 +0530

    bhbake

--- game.py
+++ game.py
@@ -7,6 +7,7 @@ engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
 engine.setProperty("rate", 170)
+icecream = MainWindow()
 
 def speak(audio):
     engine.say(audio)
