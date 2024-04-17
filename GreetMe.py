6d3c0402d7fb8567496e4052aeaffc735c10ba9f
Aryan Naik <143262856+aryannaik225@users.noreply.github.com>
2024-04-17 16:28:07 +0530

    Create GreetMe.py

---
+++ GreetMe.py
@@ -0,0 +1,27 @@
+import pyttsx3
+import datetime
+
+engine = pyttsx3.init("sapi5")
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[0].id)
+engine.setProperty("rate", 170) #speed of the bot speaking
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def greetMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<=12:
+        speak("Good Morning Sir")
+        response = "Good Morning Sir"
+    elif hour>12 and hour<18:
+        speak("Good Afternoon Sir")
+        response = "Good Afternoon Sir"
+    else:
+        speak("Good Evening Sir")
+        response = "Good Evening Sir"
+
+    speak("Please tell me how can I help you today")
+    return response + "\nPlease tell me how can I help you today"
+    
