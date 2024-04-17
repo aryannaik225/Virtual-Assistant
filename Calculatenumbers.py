96f64d4c7dcb1c304f195436d47b8f0eb6ee2e85
Aryan Naik <143262856+aryannaik225@users.noreply.github.com>
2024-04-17 16:25:46 +0530

    Create Calculatenumbers.py

---
+++ Calculatenumbers.py
@@ -0,0 +1,43 @@
+import wolframalpha
+import pyttsx3
+import speech_recognition
+
+engine = pyttsx3.init("sapi5")
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[0].id)
+rate = engine.setProperty("rate",170)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def WolfRamAlpha(query):
+    apikey = "Y9UATL-HA3824K5W7"
+    requester = wolframalpha.Client(apikey)
+    requested = requester.query(query)
+
+    try:
+        answer = next(requested.results).text
+        return answer
+    except:
+        speak("The value is not answerable")
+
+def Calc(query):
+    Term = str(query)
+    Term = Term.replace("jarvis","")
+    Term = Term.replace("multiply","*")
+    Term = Term.replace("into","*")
+    Term = Term.replace("times","*")
+    Term = Term.replace("plus","+")
+    Term = Term.replace("minus","-")
+    Term = Term.replace("divide by","/")
+    Term = Term.replace("divide","/")
+
+    Final = str(Term)
+    try:
+        result = WolfRamAlpha(Final)
+        print(f"{result}")
+        speak(result)
+
+    except:
+        speak("The value is not answerable")
