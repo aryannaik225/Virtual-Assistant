12bd8e1ce2779386bab1ff199fb887adf29939ac
Aryan Naik <143262856+aryannaik225@users.noreply.github.com>
2024-04-17 16:31:25 +0530

    Create SearchNow.py

---
+++ SearchNow.py
@@ -0,0 +1,82 @@
+import speech_recognition
+import pyttsx3
+import pywhatkit
+import wikipedia
+import webbrowser
+
+def takeCommand():
+    r = speech_recognition.Recognizer()
+    with speech_recognition.Microphone() as source:
+        print("Listening.....")
+        r.pause_threshold = 1
+        r.energy_threshold = 300
+        audio = r.listen(source, 0, 4)
+    try:
+        print("Understanding..")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"You Said: {query}\n")
+    except Exception as e:
+        print("Say that again")
+        return "None"
+    return query
+
+query = takeCommand().lower()
+
+engine = pyttsx3.init("sapi5")
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[0].id)
+engine.setProperty("rate", 170)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def searchGoogle(query):
+    if "google" in query:
+        import wikipedia as googleScrap
+        query = query.replace("jarvis", "")
+        query = query.replace("google search", "")
+        query = query.replace("google", "")
+        speak("This is what I found on Google")
+
+        try:
+            pywhatkit.search(query)
+            result = googleScrap.summary(query, 1)
+            return result
+
+        except:
+            r = "No speakable output available"
+            return r
+
+def searchYoutube(query):
+    if "youtube" in query:
+        speak("This is what I found for your search!")
+        query = query.replace("youtube search", "")
+        query = query.replace("youtube", "")
+        query = query.replace("jarvis", "")
+        web = "https://www.youtube.com/results?search_query=" + query
+        webbrowser.open(web)
+        pywhatkit.playonyt(query)
+        speak("Done, Sir")
+
+def searchWikipedia(query):
+    if "wikipedia" in query:
+        speak("Searching from Wikipedia....")
+        query = query.replace("wikipedia", "")
+        query = query.replace("search wikipedia", "")
+        query = query.replace("jarvis", "")
+        results = wikipedia.summary(query, sentences=2)
+        speak("According to Wikipedia..")
+        print(results)
+        return results
+
+def translate(query):
+    if "translate" in query:
+        query_parts = query.split("translate")
+        text_to_translate = query_parts[1].strip()
+        target_language = query_parts[2].strip()
+        # You can use translation APIs or libraries here to perform the translation
+        # For example, you can use Google Translate API or other similar services
+
+        # For demonstration, I'm just printing the translation query
+        print(f"Translating '{text_to_translate}' to {target_language}")
